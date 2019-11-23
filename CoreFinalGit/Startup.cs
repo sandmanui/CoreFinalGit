a34d541f913918ea7133c9644b11cf670743bc4b
sandeep patil <jal.sandy19864@gmail.com>
2019-11-23 16:10:25 +0000

    2nd commit

--- CoreFinalGit/Startup.cs
+++ CoreFinalGit/Startup.cs
@@ -27,7 +27,7 @@ namespace CoreFinalGit
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World! 2nd checking");
             });
         }
     }
